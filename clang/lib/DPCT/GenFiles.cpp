c7d3bebed9b934924ccb6f936673affdd4507d96
zhiming <zhiming.wang@intel.com>
2024-11-06 10:01:32 +0800

    fix a comment.

--- clang/lib/DPCT/GenFiles.cpp
+++ clang/lib/DPCT/GenFiles.cpp
@@ -1,4 +1,4 @@
-//===--------------- GenSYCLFiles.cpp -------------------------------------===//
+//===--------------- GenFiles.cpp -------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
