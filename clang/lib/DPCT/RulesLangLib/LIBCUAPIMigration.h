415eb7214cb40aada9e6c000c1dadd9a113b7df2
zhiming <zhiming.wang@intel.com>
2024-11-07 10:05:10 +0800

    more

---
+++ clang/lib/DPCT/RulesLangLib/LIBCUAPIMigration.h
@@ -0,0 +1,29 @@
+//===---------------LIBCUAPIMigration.h---------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===------------------------------------------------------------------===//
+
+#ifndef DPCT_LIBCU_API_MIGRATION_H
+#define DPCT_LIBCU_API_MIGRATION_H
+
+#include "ASTTraversal.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+
+namespace clang {
+namespace dpct {
+
+class LIBCURule : public NamedMigrationRule<LIBCURule> {
+public:
+  void registerMatcher(ast_matchers::MatchFinder &MF) override;
+  void runRule(const ast_matchers::MatchFinder::MatchResult &Result);
+
+private:
+  void processLIBCUUsingDirectiveDecl(const UsingDirectiveDecl *UDD);
+};
+} // namespace dpct
+} // namespace clang
+
+#endif // DPCT_LIBCU_API_MIGRATION_H
